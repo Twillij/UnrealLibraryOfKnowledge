825e84b7839f12c5e04241560dbdafb3b7a5203f
Will <silentwill29@gmail.com>
2024-06-15 19:56:43 +1000

    Initial commit

---
+++ Source/LibraryOfKnowledgeEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class LibraryOfKnowledgeEditorTarget : TargetRules
+{
+	public LibraryOfKnowledgeEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.Add("LibraryOfKnowledge");
+	}
+}
