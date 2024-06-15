e730d4a09966067d3af198bd5474a5ae7086cce5
Will <silentwill29@gmail.com>
2024-06-15 20:03:43 +1000

    Added Steam OSS to the settings

--- Source/LibraryOfKnowledge/LibraryOfKnowledge.Build.cs
+++ Source/LibraryOfKnowledge/LibraryOfKnowledge.Build.cs
@@ -8,7 +8,17 @@ public class LibraryOfKnowledge : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"CoreUObject",
+				"Engine",
+				"InputCore",
+				"EnhancedInput",
+				"OnlineSubsystem",
+				"OnlineSubsystemSteam"
+			});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
